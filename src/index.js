245d6ef98305a383310970e7000c27068195f444
nvwolf418 <taispringer@gmail.com>
2021-05-23 16:25:24 -0500

    Update index.js
    update app

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { StrictMode } from "react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
+
