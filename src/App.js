a869544a2be027ad0860500bfb2d473233eb55f1
nvwolf418 <taispringer@gmail.com>
2021-05-23 16:24:24 -0500

    Update App.js
    update app..js with react code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import "./styles.css";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  function myfunc(){
+    fetch('https://testsetcookie.azurewebsites.net/api/testsetcookie').then(
+      response=>{
+        return response.json();
+    })
+    console.log('The link was clicked.');
+  };
+ 
+
+export default function App() {
+  return (
+    <div className="App">
+      <button onClick={myfunc}>get cookies</button>
+      <h2 id="cookievalue">Cookie value here</h2>
+    </div>
+  );
 }
-
-export default App;
