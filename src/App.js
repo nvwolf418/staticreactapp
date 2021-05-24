26a6c9e932f329c7a2c9d096b537ec28db79e150
tspring418 <tyspring@microsoft.com>
2021-05-23 23:27:23 -0500

    remove creds

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./index.css";
 
 
   function myfunc(){
-    fetch('https://testsetcookie.azurewebsites.net/api/testsetcookie', {credentials: 'include'});
+    fetch('https://testsetcookie.azurewebsites.net/api/testsetcookie');
     console.log('The link was clicked.');
   };
  
